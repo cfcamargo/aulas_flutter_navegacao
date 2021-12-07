449cd70281fcab549c69b569b433ffeb0b95ef37
Cristian Camargo <chris.camargo2015@gmail.com>
2021-12-06 20:00:38 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/navegacao_entretelas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.navegacao_entretelas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:navegacao_entretelas/TelaSecundaria.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: TelaPrincipal(),
+  ));
+}
+
+class TelaPrincipal extends StatefulWidget {
+  const TelaPrincipal({Key? key}) : super(key: key);
+
+  @override
+  _TelaPrincipalState createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Navegação"),
+        backgroundColor: Colors.purple,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          children: [
+            RaisedButton(
+              child: Text("Ir para tela Secundaria"),
+              padding: EdgeInsets.all(15),
+              onPressed: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => TelaSecundaria()
+                    )
+                  );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
